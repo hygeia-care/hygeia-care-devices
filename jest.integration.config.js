fa44192234d25fdffd73a1e999971d79ae7d296a
Miguel <migmatgar92@gmail.com>
2024-01-23 20:14:23 +0100

    Add db test setup - not working

--- jest.integration.config.js
+++ jest.integration.config.js
@@ -160,13 +160,12 @@ const config = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: [
-    "/node_modules/",
-    "/tests/integration/"
-  ],
+  // testPathIgnorePatterns: [
+  //   "\\\\node_modules\\\\"
+  // ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
-  // testRegex: [],
+  testRegex: ["/tests/integration/"],
 
   // This option allows the use of a custom results processor
   // testResultsProcessor: undefined,
---
+++ tests/integration/db-integration.js
@@ -0,0 +1,33 @@
+const Analysis = require('../../models/analysis')
+const dbConnect = require('../../db')
+
+jest.setTimeout(30000); 
+
+describe('Analysis DB connection', () => {
+    beforeAll((done) => {
+        if (dbConnect.readyState == 1) {
+            done();
+        } else {
+            dbConnect.on("connected", () => done());
+        }
+    })
+
+    beforeEach(async () => {
+        await Analysis.deleteMany({});
+    });
+
+    it('writes a analysis in the DB', async () =>{
+        const analysis = new Analysis({value:'123456', measurement: 'abc'});
+        await analysis.save();
+        analysisReturned = await Analysis.find();
+        expect(analysisReturned).toBeArrayOfSize(1);
+
+    });
+
+    afterAll(async () => {
+        if (dbConnect.readyState == 1) {
+            await dbConnect.dropDatabase();
+            await dbConnect.close();
+        }
+    });
+});
